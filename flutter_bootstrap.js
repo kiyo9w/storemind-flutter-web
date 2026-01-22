18cf4ae06848e59ecbf4bf7b959296f7f300519b
kiyo9w <ngokapikapi@gmail.com>
2026-01-22 20:14:12 +0700

    change tab's icon and add multilingual for top banner and pop up

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1847399256"
+    serviceWorkerVersion: "60142654"
   }
 });
