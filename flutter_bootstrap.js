f7d076055ba59151f9fd0bfa26a4aedcf3db9fd6
kiyo9w <ngokapikapi@gmail.com>
2026-09-03 14:00:32 +0900

    deploy: bundled plan product photos

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"a804b261645ef8c13eb3d5c44a5c2fb0340c55
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1690813783" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1180474509" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
